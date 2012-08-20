95ecb648fc566e050bca4911b3a10fa2dca187f4
Aron Woost <aron@springworks.de>
2012-08-20 09:29:38 +0200

    first commit

---
+++ savespecsfile.py
@@ -0,0 +1,14 @@
+import sublime, sublime_plugin, ntpath
+
+# does not work sublime_plugin.WindowCommand, no clue why
+class SaveSpecsFileCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		# all open view (i.e. tabs)
+		views = self.view.window().views()
+		for v in views:
+			# check if the filename start with "test."
+			if ntpath.basename(v.file_name())[0:5] == "test.":
+				# print(ntpath.basename(v.file_name()))
+				v.run_command("save")
+
+# Sublime Text 2 command that triggers the mocha.js watcher by saving the specs file.
